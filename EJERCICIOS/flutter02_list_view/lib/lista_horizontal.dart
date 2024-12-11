dda0e851e5eeed31fdcbf16e931c691f09dbb79e
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-12-11 22:14:27 +0100

    Jojos conseguidos

--- EJERCICIOS/flutter02_list_view/lib/lista_horizontal.dart
+++ EJERCICIOS/flutter02_list_view/lib/lista_horizontal.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 
 class ListaHorizontal extends StatelessWidget {
@@ -19,6 +21,7 @@ class ListaHorizontal extends StatelessWidget {
             Container(
               padding: const EdgeInsets.only(top: 75.0, left: 40.0),
               child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
 
                   Text("Mejores protas de Jojo's\nBizarre Adventure",
@@ -29,11 +32,14 @@ class ListaHorizontal extends StatelessWidget {
                     ),
                   ),
 
-                  Text("Diciembre 2024",
+                  SizedBox(height: 10),
+
+                  Text("¡De la primera parte a la quinta!",
+                  textAlign: TextAlign.start,
                     style: TextStyle(
-                      fontSize: 12,
+                      fontSize: 15,
                       color: Color.fromARGB(255, 122, 122, 122),
-                      //fontWeight: FontWeight.w900
+                      fontWeight: FontWeight.w300
                     )
                   ),
 
@@ -42,63 +48,146 @@ class ListaHorizontal extends StatelessWidget {
             ),
 
             SizedBox(
-              height: 670,
-              child: ListView(
-                key: GlobalKey(),
-                scrollDirection: Axis.horizontal, // por defecto es vertical
-                children: const [
-                  Column(
-                    children: [
-                      Image(
-                        image: AssetImage('assets/Jojo.jpg'), width: 200
-                      ),
-
-                      Text("Diciembre 2024",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Color.fromARGB(255, 122, 122, 122),
-                          //fontWeight: FontWeight.w900
-                        )
-                      ),
-                    ]
-                  ),
               
-                  Column(
-                    children: [
-                      Image(
-                        image: AssetImage('assets/Joseph.jpg'), width: 200
-                      ),
+              height: 700,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
 
-                      Text("Diciembre 2024",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Color.fromARGB(255, 122, 122, 122),
-                          //fontWeight: FontWeight.w900
-                        )
-                      ),
-                    ]
+                  Container(
+                    padding: const EdgeInsets.only(top: 40.0, left: 40.0),
+                    height: 600,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, // Asegura que el contenido no desborde
+                      children: [
+                        Image.asset('assets/jonathan.jpg', 
+                          width: 150, height: 550, 
+                          scale: 1, // Reduce el factor de escala interno para hacerla más grande
+                          fit: BoxFit.cover,
+                          
+                        ),
+                        const SizedBox(height: 10), // Espaciado entre imagen y texto
+                        const Text(
+                          "Jonathan\nJoestar",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
 
-                  Column(
-                    children: [
-                      Image(
-                        image: AssetImage('assets/Jojo.jpg'), width: 200
-                      ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 40.0, left: 20.0),
+                    height: 600,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, // Asegura que el contenido no desborde
+                      children: [
+                        Image.asset('assets/joseph.jpg', 
+                          width: 150, height: 550, 
+                          scale: 1, // Reduce el factor de escala interno para hacerla más grande
+                          fit: BoxFit.cover,
+                          
+                        ),
+                        const SizedBox(height: 10), // Espaciado entre imagen y texto
+                        const Text(
+                          "Joseph\nJoestar",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
 
-                      Text("Diciembre 2024",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Color.fromARGB(255, 122, 122, 122),
-                          //fontWeight: FontWeight.w900
-                        )
-                      ),
+                  Container(
+                    padding: const EdgeInsets.only(top: 40.0, left: 20.0),
+                    height: 600,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, // Asegura que el contenido no desborde
+                      children: [
+                        Image.asset('assets/jotaro.jpg', 
+                          width: 150, height: 550, 
+                          scale: 1, // Reduce el factor de escala interno para hacerla más grande
+                          fit: BoxFit.cover,
+                          
+                        ),
+                        const SizedBox(height: 10), // Espaciado entre imagen y texto
+                        const Text(
+                          "Jotaro\nKujo",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
 
-                    ]
+                  Container(
+                    padding: const EdgeInsets.only(top: 40.0, left: 20.0),
+                    height: 600,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, // Asegura que el contenido no desborde
+                      children: [
+                        Image.asset('assets/josuke.jpg', 
+                          width: 150, height: 550, 
+                          scale: 1, // Reduce el factor de escala interno para hacerla más grande
+                          fit: BoxFit.cover,
+                          
+                        ),
+                        const SizedBox(height: 10), // Espaciado entre imagen y texto
+                        const Text(
+                          "Josuke\nHagashikata",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    padding: const EdgeInsets.only(top: 40.0, left: 20.0, right: 40.0),
+                    height: 600,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, // Asegura que el contenido no desborde
+                      children: [
+                        Image.asset('assets/giorno.jpg', 
+                          width: 150, height: 550, 
+                          scale: 1, // Reduce el factor de escala interno para hacerla más grande
+                          fit: BoxFit.cover,
+                          
+                        ),
+                        const SizedBox(height: 10), // Espaciado entre imagen y texto
+                        const Text(
+                          "Giorno\nGiovanna",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w900
+                          ),
+                        ),
+                      ],
+                    ),
                   )
                   
-                ]
-              )
-            )
+                ],
+              ),
+            ),
+
           ],
         )
       )
